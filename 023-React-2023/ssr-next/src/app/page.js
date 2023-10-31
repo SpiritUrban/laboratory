f037b1524f2024d4946b11aa7cc591cd5fc686c3
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2023-10-31 02:40:33 +0200

    rect; event dildo examples

---
+++ 023-React-2023/ssr-next/src/app/page.js
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing 123&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+      
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+    </main>
+  )
+}
