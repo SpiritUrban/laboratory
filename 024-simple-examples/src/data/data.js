9bc4de696e4fde78cae96b418cddab65bd17f280
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2023-11-03 13:40:57 +0200

    add scene nav

--- 024-simple-examples/src/data/data.js
+++ 024-simple-examples/src/data/data.js
@@ -24,3 +24,58 @@ let part = str.substr(7);
     },
   ],
 };
+
+const dataX = [
+  {
+    header: `JavaScript String: substr()`,
+    portions: [
+      {
+        text: [
+          "substr() is similar to slice().",
+          `  The difference is that the second parameter ... 
+specifies the length of the extracted part.`,
+        ],
+        exaple: `let str = "Apple, Banana, Kiwi";
+let part = str.substr(7, 6);
+// part -> 'Banana'`,
+      },
+      {
+        text: [
+          `If you omit the second parameter, 
+substr() will slice out the rest of the string.`,
+          `If the first parameter is negative, 
+the position counts from the end of the string.`,
+        ],
+        exaple: `let str = "Apple, Banana, Kiwi";
+let part = str.substr(7);
+// part -> 'Banana, Kiwi'`,
+      },
+    ],
+  },
+  {
+    header: `JavaScript String: substr()2`,
+    portions: [
+      {
+        text: [
+          "substr() is similar to slice().",
+          `  The difference is that the second parameter ... 
+specifies the length of the extracted part.`,
+        ],
+        exaple: `let str = "Apple, Banana, Kiwi";
+let part = str.substr(7, 6);
+// part -> 'Banana'`,
+      },
+      {
+        text: [
+          `If you omit the second parameter, 
+substr() will slice out the rest of the string.`,
+          `If the first parameter is negative, 
+the position counts from the end of the string.`,
+        ],
+        exaple: `let str = "Apple, Banana, Kiwi";
+let part = str.substr(7);
+// part -> 'Banana, Kiwi'`,
+      },
+    ],
+  },
+];
