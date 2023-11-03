21461df19f0f41ae3ef65d83f1b15521ab9f0865
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2023-11-03 14:14:37 +0200

    add yaml

--- 024-simple-examples/src/data/data.js
+++ 024-simple-examples/src/data/data.js
@@ -1,4 +1,4 @@
-const data4 = {
+const dataY = {
   header: `JavaScript String: substr()`,
   portions: [
     {
@@ -79,3 +79,51 @@ let part = str.substr(7);
     ],
   },
 ];
+
+const ya = `
+- header: "JavaScript String: substr()"
+  portions:
+    - text:
+        - substr() is similar to slice().
+        - |
+          The difference is that the second parameter ...
+          specifies the length of the extracted part.
+      example: |
+        let str = "Apple, Banana, Kiwi";
+        let part = str.substr(7, 6);
+        // part -> 'Banana'
+    - text:
+        - |
+          If you omit the second parameter, 
+          substr() will slice out the rest of the string.
+        - |
+          If the first parameter is negative, 
+          the position counts from the end of the string.
+      example: |
+        let str = "Apple, Banana, Kiwi";
+        let part = str.substr(7);
+        // part -> 'Banana, Kiwi'
+
+- header: "JavaScript String: substr()2"
+  portions:
+    - text:
+        - |
+          substr() is similar to slice().
+          The difference is that the second parameter ...
+          specifies the length of the extracted part.
+      example: |
+        let str = "Apple, Banana, Kiwi";
+        let part = str.substr(7, 6);
+        // part -> 'Banana'
+    - text:
+        - |
+          If you omit the second parameter, 
+          substr() will slice out the rest of the string.
+          If the first parameter is negative, 
+          the position counts from the end of the string.
+      example: |
+        let str = "Apple, Banana, Kiwi";
+        let part = str.substr(7);
+        // part -> 'Banana, Kiwi'
+
+`;
